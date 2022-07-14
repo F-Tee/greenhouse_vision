72a9798bc5b9f35ed4178e0190c99515ea4e9066
F-Tee <JoshT626@hotmail.co.uk>
2022-07-14 18:03:15 +0100

    Added ability to calibrate camera by saving images of chessboard.

--- OpenCV/OpenCV.cpp
+++ OpenCV/OpenCV.cpp
@@ -64,7 +64,117 @@ void createArucoMarkers() {
 	}
 }
 
+void cameraCalibration(std::vector<Mat> calibrationImages, Size boardSize, float squareEdgeLength, Mat& cameraMatrix, Mat& distanceCoefficients) {
+	std::vector<std::vector<Point2f>> checkerboardImageSpacePoints;
+	getChessboardCorners(calibrationImages, checkerboardImageSpacePoints, false);
+
+	std::vector<std::vector<Point3f>> worldSpaceCornerPoints(1);
+
+	createKnownBoardPosition(boardSize, squareEdgeLength, worldSpaceCornerPoints[0]);
+	worldSpaceCornerPoints.resize(checkerboardImageSpacePoints.size(), worldSpaceCornerPoints[0]);
+
+	std::vector<Mat> rVectors, tVectors;
+	distanceCoefficients = Mat::zeros(8, 1, CV_64F);
+
+	calibrateCamera(worldSpaceCornerPoints, checkerboardImageSpacePoints, boardSize, cameraMatrix, distanceCoefficients, rVectors, tVectors);
+}
+
+bool saveCameraCalibration(std::string name, Mat cameraMatrix, Mat distanceCoefficients) {
+	std::ofstream outStream(name);
+	if (outStream) {
+		uint16_t rows = cameraMatrix.rows;
+		uint16_t columns = cameraMatrix.cols;
+
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < columns; c++) {
+				double value = cameraMatrix.at<double>(r, c);
+				outStream << value << std::endl;
+			}
+		}
+
+		rows = distanceCoefficients.rows;
+		columns = distanceCoefficients.cols;
+
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < columns; c++) {
+				double value = distanceCoefficients.at<double>(r, c);
+				outStream << value << std::endl;
+			}
+		}
+
+		outStream.close();
+		return true;
+	}
+
+	return false;
+}
+
 int main(int argv, char** argc)
 {
-	createArucoMarkers();
+	Mat frame;
+	Mat drawToFrame;
+
+	Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
+
+	Mat distanceCoefficients;
+
+	std::vector<Mat> savedImages;
+
+	std::vector<std::vector<Point2f>> markerCorners, rejectedCandidates;
+
+	VideoCapture vid(0);
+
+	if (!vid.isOpened()) {
+		return 0;
+	}
+
+	int framesPerSecond = 20;
+
+	namedWindow("Webcam", WINDOW_AUTOSIZE);
+
+	while (true) {
+		if (!vid.read(frame)) {
+			break;
+		}
+
+		std::vector<Vec2f> foundPoints;
+		bool found = false;
+
+		found = findChessboardCorners(frame, chessboardDimensions, foundPoints, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE | CALIB_CB_FAST_CHECK);
+		frame.copyTo(drawToFrame);
+		drawChessboardCorners(drawToFrame, chessboardDimensions, foundPoints, found);
+		if (found) {
+			imshow("Webcam", drawToFrame);
+		}
+		else {
+			imshow("Webcam", frame);
+		}
+		char character = waitKey(1000 / framesPerSecond);
+
+		switch (character) {
+		case ' ':
+			// space
+			// saving image
+			if (found) {
+				Mat temp;
+				frame.copyTo(temp);
+				savedImages.push_back(temp);
+			}
+			break;
+		case 13:
+			// enter key
+			// start calib
+			if (savedImages.size() > 15) {
+				cameraCalibration(savedImages, chessboardDimensions, calibrationSquareDimension, cameraMatrix, distanceCoefficients);
+				saveCameraCalibration("Camera_Calibration", cameraMatrix, distanceCoefficients);
+			}
+			break;
+		case 27:
+			// exit
+			return 0;
+			break;
+		}
+	}
+
+	return 0;
 }
